0e880199fee4fb1a11acd3d3e7e68cf63baed0a2
mamun2519 <juborajislam46@gmail.com>
2024-11-27 21:40:11 +0600

    added to owner

--- File-Modification.sh
+++ File-Modification.sh
@@ -1,4 +1,12 @@
-Create a file named example.txt in your home directory.
+1. Create a file named example.txt in your home directory.
+command=> touch  ~/example.txt
+
 Change the owner of example.txt to a user named student
+command=> sudo chown student ~/example.txt
+
+
 Change the group of example.txt to a group named students.
+command=>
+
 Verify the changes using appropriate commands.
+command=>
