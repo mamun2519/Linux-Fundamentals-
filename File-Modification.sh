01addc29a245cf3aebec93df1bc2a743d75835db
mamun2519 <juborajislam46@gmail.com>
2024-11-27 21:35:57 +0600

    file modification

---
+++ File-Modification.sh
@@ -0,0 +1,4 @@
+Create a file named example.txt in your home directory.
+Change the owner of example.txt to a user named student
+Change the group of example.txt to a group named students.
+Verify the changes using appropriate commands.
