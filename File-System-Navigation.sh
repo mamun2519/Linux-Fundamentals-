b3323d5f4578da340f5eed0cfa2d423b6fe45a66
mamun2519 <juborajislam46@gmail.com>
2024-11-27 21:08:31 +0600

    added to file system command

---
+++ File-System-Navigation.sh
@@ -0,0 +1,7 @@
+File System Navigation
+- List the contents of the home directory.
+command=>  ls ~
+Change the current directory to /var/log and list its contents.
+command=>
+Find and display the path to the bash executable using the which command.
+Find current shell
