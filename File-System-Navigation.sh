c69c5761b20085a8a2eca8e3f2d5ccd069b2fb8e
mamun2519 <juborajislam46@gmail.com>
2024-11-27 21:15:15 +0600

    complete file system navigation

--- File-System-Navigation.sh
+++ File-System-Navigation.sh
@@ -2,6 +2,11 @@ File System Navigation
 - List the contents of the home directory.
 command=>  ls ~
 Change the current directory to /var/log and list its contents.
-command=>
+command=>cd cd /var/log && ls
 Find and display the path to the bash executable using the which command.
+command=> which bash
+outPut => /usr/bin/bash
 Find current shell
+command=> echo $SHELL
+outPut=>/bin/bash
+
