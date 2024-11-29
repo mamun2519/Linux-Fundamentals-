7f825026bcce541a9d8789933037ee8c0a79e6cb
mamun2519 <juborajislam46@gmail.com>
2024-11-29 18:57:07 +0600

    added to file permisson command

--- User-add/modify.sh
+++ User-add/modify.sh
@@ -1,28 +1,36 @@
 1. Create a new user named developer.
-command=>
+command=> sudo useradd developer
+
 
 
 2. Set the home directory of the user developer to /home/developer_home.
-command=>
+command=> sudo usermod -d /home/developer_home -m developer
+
 
 
 3. Assign the shell /bin/sh to the user developer.
-command=>
+command=> sudo usermod -s /bin/sh developer
+
 
 
 4. Verify the new user's information.
-command=>
+command=> getent passwd developer
+
 
 
 5. Change the username of the user developer to devuser.
-command=>
+command=> sudo usermod -l devuser developer
+
 
 6. Add devuser to a group named devgroup.
-command=>
+command 1=> sudo groupadd devgroup
+command 2 => sudo usermod -aG devgroup devuser
+
 
 
 7. Set the password of devuser to devpass. ( hint: use passwd command. Run passwd --help to see available options)
-command=>
+command=> sudo passwd devuser
+
 
 8. Verify the changes made to the user.
-command=>
+command=> getent passwd devuser
