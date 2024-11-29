0b40ae8f7646a23f1736b9c7904ca4cceaecc01e
mamun2519 <juborajislam46@gmail.com>
2024-11-29 18:59:41 +0600

    added to file permisson command

---
+++ Hard-SoftLink.sh
@@ -0,0 +1,10 @@
+Create a file named original.txt in your home directory.
+Create a symbolic link named softlink.txt pointing to original.txt.
+Verify the symbolic link and ensure it points to the correct file.
+Delete the original file original.txt and observe the status of the symbolic link.
+Create a file named datafile.txt in your home directory.
+Create a hard link named hardlink.txt pointing to datafile.txt.
+Verify the hard link and ensure it correctly points to the file.
+Check the inode of both datafile.txt and hardlink.txt
+Delete the original file datafile.txt and observe the status of the hard link.
+Find all .txt files in your home directory. ( use find command. Run find --help for usage)
