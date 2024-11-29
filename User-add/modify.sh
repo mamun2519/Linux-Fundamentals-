c2ed9b8719136b8be214d9ecb0c3d32ccf0ea677
mamun2519 <juborajislam46@gmail.com>
2024-11-29 18:49:36 +0600

    added to file permisson command

---
+++ User-add/modify.sh
@@ -0,0 +1,8 @@
+Create a new user named developer.
+Set the home directory of the user developer to /home/developer_home.
+Assign the shell /bin/sh to the user developer.
+Verify the new user's information.
+Change the username of the user developer to devuser.
+Add devuser to a group named devgroup.
+Set the password of devuser to devpass. ( hint: use passwd command. Run passwd --help to see available options)
+Verify the changes made to the user.
